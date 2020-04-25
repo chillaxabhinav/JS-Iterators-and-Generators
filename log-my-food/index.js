e5c7836bab44287f767ece6f15e3b921a923a5d9
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-25 18:43:32 +0530

    Custom itartors

--- log-my-food/index.js
+++ log-my-food/index.js
@@ -2,20 +2,62 @@ const axios = require('axios');
 
 const readline = require('readline').createInterface({
     input : process.stdin,
-    output : process.stdout
+    output : process.stdout,
+    prompt : 'enter command > '
 });
 
-readline.question(`What you would like to log today\n`,async (item) => {
-    const { data } = await axios.get('http://localhost:3001/food');
-    const it = data[Symbol.iterator]();
-    let position = it.next();
-    while(!position.done){
-        const food = position.value.name;
-        if(food === item){
-            console.log(`${item} has ${position.value.calories} calories.`);
-            break;
+readline.prompt();
+readline.on('line', line => {
+    switch(line.trim()){
+        case 'list vegan foods':
+            {
+                axios.get('http://localhost:3001/food').then( ({data}) => {
+                    let idx = 0;
+                    const veganOnly = data.filter(ele => {
+                        return ele.dietary_preferences.includes('vegan');
+                    });
+                    const veganIterable = {
+                        [Symbol.iterator](){
+
+                            return {
+                                [Symbol.iterator](){
+                                    return this;
+                                },
+                                next(){
+                                    const current = veganOnly[idx];
+                                    idx++;
+                                    if(current){
+                                        return {value : current, done : false};
+                                    }
+                                    else{
+                                        return {value : current, done : true};
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    for(let val of veganIterable){
+                        console.log(val.name);
+                    }
+                    readline.prompt();
+                })
+            }
+        case 'log' : {
+            readline.question(`What you would like to log today\n`, async (item) => {
+                const { data } = await axios.get('http://localhost:3001/food');
+                const it = data[Symbol.iterator]();
+                let position = it.next();
+                while (!position.done) {
+                    const food = position.value.name;
+                    if (food === item) {
+                        console.log(`${item} has ${position.value.calories} calories.`);
+                        break;
+                    }
+                    position = it.next();
+                }
+                readline.prompt();
+            })
         }
-        position = it.next();
     }
-    readline.close();
 })
+
